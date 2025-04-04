5216d83fd1a2b74fc492c393a65c154db87b1b68
Vlad <88883584+0MysticGuy0@users.noreply.github.com>
2025-04-04 16:38:15 +0900

    Created project

---
+++ src/main/java/com/vlat/RSTECHtestovoe/RstecHtestovoeApplication.java
@@ -0,0 +1,13 @@
+package com.vlat.RSTECHtestovoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RstecHtestovoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RstecHtestovoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlat/RSTECHtestovoe/RstecHtestovoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlat.RSTECHtestovoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RstecHtestovoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
