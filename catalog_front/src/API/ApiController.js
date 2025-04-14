b8381cf576aa4b444790c11d77481ec9ce2a2984
Vlad <88883584+0MysticGuy0@users.noreply.github.com>
2025-04-14 18:31:58 +0900

    fixed

--- catalog_front/src/API/ApiController.js
+++ catalog_front/src/API/ApiController.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default class APIcontroller{
-    #API_ADDRESS = process.env.API_URL
+    #API_ADDRESS = process.env.REACT_APP_API_URL
     #API_CATEGORY_ADDRESS = this.#API_ADDRESS+"category/"
     #API_PRODUCT_ADDRESS = this.#API_ADDRESS+"product/"
     #API_IMAGE_ADDRESS = this.#API_ADDRESS+"product-image/"
